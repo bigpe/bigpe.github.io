f914ab7eb50ded9579e028c3828b51ebd3cd2dcc
Alex <bigpewm@gmail.com>
2020-08-14 20:20:45 +0300

    Simple Version

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 function loadFReviews() {
     $.ajax({
         type: "GET",
-        url: "https://www.freelancer.com/api/projects/0.1/reviews/?role=freelancer&to_users[]=46624288&compact=true",
+        url: "https://www.freelancer.com/api/projects/0.1/reviews/?role=freelancer&to_users[]=46624288&project_details=true&contest_details=true&project_job_details=true&contest_job_details=true&project_deleted=false&webapp=1&compact=true&new_errors=true",
         complete: function (data) {
-            let reviews = JSON.parse(data.responseText)['result']['reviews'];
+            let parsedData = JSON.parse(data.responseText)['result'];
+            let reviews = parsedData['reviews'];
             let leBlock = document.getElementById("latestExperience");
             let d = 1;
             for (let r in reviews) {
+                let projectId = reviews[r]['project_id'];
+                let skills = [];
+                for (let s in parsedData['projects'][projectId]['jobs']){
+                    if (parsedData['projects'][projectId]['jobs'][s]['name'] != 'Hire me')
+                        skills.push(parsedData['projects'][projectId]['jobs'][s]['name']);
+                }
                 let projectName = reviews[r]['review_context']['context_name'];
                 let projectURL = "https://freelancer.com/" + reviews[r]['review_context']['seo_url'];
                 let rating = reviews[r]['rating'];
@@ -16,13 +23,17 @@ function loadFReviews() {
                 let revBlock = document.createElement("div");
                 revBlock.setAttribute("class", "revealator-slideright revealator-partially-above revealator-once revealator-delay" + d++);
                 let revTitle = document.createElement("div");
+                let i = parseInt(r);
+                i++;
                 revTitle.setAttribute("class", "card-header bg-light text-dark shadow-sm rounded-0 font-weight-bold");
-                revTitle.innerHTML = "<a href='" + projectURL + "' target='_blank'>" + projectName + "</a>" + " " + rating
-                    + " <i class='fas fa-star text-warning'></i>"
-                    + " <span class='text-success'>" + paid + currency + "</span>";
+                revTitle.innerHTML = "<a href='" + projectURL + "' target='_blank'>"
+                    + "Business project#" + i + " <sup><i class=\"fas fa-external-link-alt\"></i></a></sup>"
+                    + " <sup class='text-secondary'>" + paid + currency + "</sup>";
                 let revBody = document.createElement("div");
-                revBody.setAttribute("class", "border-bottom card-body bg-light small text-secondary font-weight-bold");
-                revBody.innerHTML = description;
+                revBody.setAttribute("class", "border-bottom small card-body text-light bg-light font-weight-bold");
+                for (let s in skills){
+                    revBody.innerHTML += "<span class='d-inline-flex bg-dark small font-weight-bold shadow-sm rounded p-1 m-1'>"+ skills[s] +"</span>"
+                }
                 revBlock.append(revTitle, revBody);
                 leBlock.append(revBlock);
             }
