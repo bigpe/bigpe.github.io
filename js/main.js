a4f1d23ac79f72446b96385186a1f833b0bb13b2
Alex <bigpewm@gmail.com>
2021-01-11 21:22:56 +1000

    Fix: Adapt blocks

--- js/main.js
+++ js/main.js
@@ -57,9 +57,10 @@ function loadScripts(){
 		sendAjax(`${localHost}${toLoadScripts[i]}`, 'head', loadScript);
 }
 function loadStyles(){
-	changeLoadingBar(`Loading Static Styles`);
+	// changeLoadingBar(`Loading Static Styles`);
 	for (let i in toLoadStyles)
 		sendAjax(`${localHost}${toLoadStyles[i]}`, 'head', loadStyle);
+	$('.loadingBar').addClass('d-block');
 }
 function lazyLoadBlocks(){
 	toLoadBlocks = $('.loadBlock');
