a76ff96b6577f64e860d5fdc11cdd5dc2786d2e0
Alex <bigpewm@gmail.com>
2021-01-19 01:13:31 +1000

    Feature: Frog Animation

--- js/main.js
+++ js/main.js
@@ -107,7 +107,7 @@ function loadBlock(content, blockId, cache=false){
 	}
 	setTimeout(function () {
 		$(`#${blockId}`).html(content);
-	}, 700);
+	}, 600);
 	saveLoading();
 }
 function saveLoading(){
